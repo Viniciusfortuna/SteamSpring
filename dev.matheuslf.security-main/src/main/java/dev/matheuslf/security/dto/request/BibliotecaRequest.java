9ef5aa714bea09850211917d0c8288870dabc765
vitorhgtt <61064128+vitorhgtt@users.noreply.github.com>
2025-11-18 21:22:59 -0300

    Delete dev.matheuslf.security-main/src/main/java/dev/matheuslf/security/dto/request/BibliotecaRequest.java

--- dev.matheuslf.security-main/src/main/java/dev/matheuslf/security/dto/request/BibliotecaRequest.java
+++
@@ -1,9 +0,0 @@
-package dev.matheuslf.security.dto.request;
-
-import jakarta.validation.constraints.NotNull;
-import java.util.Set;
-
-public record BibliotecaRequest(
-        @NotNull(message = "Usuário é obrigatório") Long userId,
-        Set<Long> jogosIds
-) {}
