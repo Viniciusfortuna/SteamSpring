90f8066c4274ad5011c790bbb3d49ee9bb126efc
vitorhgtt <61064128+vitorhgtt@users.noreply.github.com>
2025-11-18 21:26:29 -0300

    Delete dev.matheuslf.security-main/src/main/java/dev/matheuslf/security/repository/BibliotecaRepository.java

--- dev.matheuslf.security-main/src/main/java/dev/matheuslf/security/repository/BibliotecaRepository.java
+++
@@ -1,7 +0,0 @@
-package dev.matheuslf.security.repository;
-
-import dev.matheuslf.security.entity.Biblioteca;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BibliotecaRepository extends JpaRepository<Biblioteca, Long> {
-}
